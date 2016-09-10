ef9f4d31102f10ab1afe28ac7bc23d48edc0d8e2
mknbrown <mknbrown@Jacksons-MacBook-Air.local>
2016-09-10 17:29:02 -0500

    Created basic iOS UI with three tabs: People; Vehicles; and Other Notes Put basic fields on tabs

---
+++ Poiema.iOS/People.cs
@@ -0,0 +1,48 @@
+﻿using System;
+
+using UIKit;
+
+namespace Poiema.iOS
+{
+	public partial class People : UIViewController
+	{
+		protected People(IntPtr handle) : base(handle)
+		{
+			// Note: this .ctor should not contain any initialization logic.
+		}
+
+		public override void ViewDidLoad()
+		{
+			base.ViewDidLoad();
+
+			UIPickerView racePicker = new UIPickerView();
+			racePicker.Model = new RacePickerModel();
+		}
+
+		public class RacePickerModel : UIPickerViewModel
+		{
+			public override nint GetComponentCount(UIPickerView pickerView)
+			{
+				return 1;
+			}
+
+			public override nint GetRowsInComponent(UIPickerView pickerView, nint component)
+			{
+				return 5;
+			}
+
+			public override string GetTitle(UIPickerView pickerView, nint row, nint component)
+			{
+				return "Test " + row.ToString();
+			}
+
+		}
+
+		public override void DidReceiveMemoryWarning()
+		{
+			base.DidReceiveMemoryWarning();
+			// Release any cached data, images, etc that aren't in use.
+		}
+	}
+}
+
